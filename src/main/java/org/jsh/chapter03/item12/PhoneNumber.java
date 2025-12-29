b4ce4a8c2de41551e41e620301bdf62306728ba4
jshjshjshjsh <cj456456@gmail.com>
2025-12-29 21:58:11 +0900

    feat: add Before code

---
+++ src/main/java/org/jsh/chapter03/item12/PhoneNumber.java
@@ -0,0 +1,34 @@
+package org.jsh.chapter03.item12;
+
+// Item 11에서 완성한 코드를 패키지만 바꿔서 가져옵니다.
+import java.util.Objects;
+
+public class PhoneNumber {
+    private final short areaCode, prefix, lineNum;
+
+    public PhoneNumber(int areaCode, int prefix, int lineNum) {
+        this.areaCode = rangeCheck(areaCode, 999, "area code");
+        this.prefix   = rangeCheck(prefix,   999, "prefix");
+        this.lineNum  = rangeCheck(lineNum, 9999, "line num");
+    }
+
+    private static short rangeCheck(int val, int max, String arg) {
+        if (val < 0 || val > max)
+            throw new IllegalArgumentException(arg + ": " + val);
+        return (short) val;
+    }
+
+    @Override public boolean equals(Object o) {
+        if (o == this) return true;
+        if (!(o instanceof PhoneNumber)) return false;
+        PhoneNumber pn = (PhoneNumber)o;
+        return pn.lineNum == lineNum && pn.prefix == prefix
+                && pn.areaCode == areaCode;
+    }
+
+    @Override public int hashCode() {
+        return Objects.hash(areaCode, prefix, lineNum);
+    }
+
+    // toString() 없음 -> Object.toString() 사용됨
+}
