52d7a09772e1fcf3cf55e970b010a42dfa35b55d
jshjshjshjsh <cj456456@gmail.com>
2025-12-29 22:56:07 +0900

    refactor: 상속제거 후 컴포지션 적용

--- src/main/java/org/jsh/chapter04/item18/InstrumentedHashSet.java
+++ src/main/java/org/jsh/chapter04/item18/InstrumentedHashSet.java
@@ -2,27 +2,29 @@ package org.jsh.chapter04.item18;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Set;
 
 // 상속을 잘못 사용한 예
-public class InstrumentedHashSet<E> extends HashSet<E> {
+public class InstrumentedHashSet<E> {
     // 추가된 원소의 수
     private int addCount = 0;
+    private final Set<E> set;
 
-    public InstrumentedHashSet() {
+    public InstrumentedHashSet(Set<E> set) {
+        this.set = set;
     }
 
-    @Override
     public boolean add(E e) {
         addCount++;
-        return super.add(e);
+        return set.add(e);
     }
 
-    @Override
     public boolean addAll(Collection<? extends E> c) {
         addCount += c.size();
-        // 여기서 문제 발생!
-        // HashSet의 addAll은 내부적으로 add를 호출함 -> addCount가 또 올라감 (Double Counting)
-        return super.addAll(c);
+        // 상속과 달리 set.addAll()이 내부적으로 set.add()를 호출하더라도
+        // 그것은 저쪽 set 객체의 add이지, InstrumentedSet의 add가 아님
+        // 따라서 addCount가 중복으로 증가하지 않음
+        return set.addAll(c);
     }
 
     public int getAddCount() {
