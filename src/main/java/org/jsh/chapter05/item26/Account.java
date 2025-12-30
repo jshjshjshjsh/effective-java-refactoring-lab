18658c450ddd0f9ee2f7ab171a1c63d3bcb607af
jshjshjshjsh <cj456456@gmail.com>
2025-12-30 10:41:26 +0900

    refactor: Raw Type제거하여 타입을 명시적으로 제한

--- src/main/java/org/jsh/chapter05/item26/Account.java
+++ src/main/java/org/jsh/chapter05/item26/Account.java
@@ -4,16 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Account {
-    // Bad: Raw Type 사용 (무엇이든 들어갈 수 있음)
-    private final List history = new ArrayList();
+    // Good: Raw Type 제거하여 타입 제한
+    private final List<String> history = new ArrayList<String>();
 
-    // 아무거나 다 받음
-    public void addHistory(Object record) {
+    // String만 받음
+    public void addHistory(String record) {
         history.add(record);
     }
 
     // 꺼낼 때 String으로 형변환 (여기서 터짐)
     public String getHistory(int index) {
-        return (String) history.get(index);
+        return history.get(index);
     }
 }
--- src/test/java/org/jsh/chapter05/item26/AccountTest.java
+++ src/test/java/org/jsh/chapter05/item26/AccountTest.java
@@ -15,12 +15,13 @@ class AccountTest {
 
         // 실수로 String이 아니라 Integer를 넣음
         // Raw Type이라서 컴파일 에러가 안 남! (폭탄 설치 완료)
-        account.addHistory(10000);
+        account.addHistory("20000");
 
         // When & Then
         // 꺼낼 때 (String)으로 캐스팅하다가 터짐
         // After 리팩토링 후에는: 아예 account.addHistory(10000) 부분에서 컴파일 에러가 나야 함.
-        assertThatThrownBy(() -> account.getHistory(1))
-                .isInstanceOf(ClassCastException.class);
+        //assertThatThrownBy(() -> account.getHistory(1))
+        //        .isInstanceOf(ClassCastException.class);
+        assertThat(account.getHistory(1)).isEqualTo("20000");
     }
 }
