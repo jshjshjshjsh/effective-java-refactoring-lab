4dcbeef98eca5274ba81346295ed42260f9b1e3f
jshjshjshjsh <cj456456@gmail.com>
2025-12-30 21:33:44 +0900

    refactor: 람다로 리팩토링

--- src/main/java/org/jsh/chapter05/item34/Operation.java
+++ src/main/java/org/jsh/chapter05/item34/Operation.java
@@ -1,20 +1,24 @@
 package org.jsh.chapter05.item34;
 
-public class Operation {
-    // Bad: 정수 열거 패턴 - 타입 안전성 없음, 표현력 부족
-    public static final int PLUS = 0;
-    public static final int MINUS = 1;
-    public static final int TIMES = 2;
-    public static final int DIVIDE = 3;
+import java.util.function.DoubleBinaryOperator;
+
+public enum Operation {
+    // Good: 각 인스턴스 필드에 람다를 할당
+    PLUS("+", (x, y) -> x + y),
+    MINUS("-", (x, y) -> x - y),
+    TIMES("*", (x, y) -> x * y),
+    DIVIDE("/", (x, y) -> x / y);
+
+    private final String symbol;
+    private final DoubleBinaryOperator op;
+
+    Operation(String symbol, DoubleBinaryOperator op) {
+        this.symbol = symbol;
+        this.op = op;
+    }
 
     // 연산을 수행하는 메서드 (상수가 추가되면 여기도 수정해야 함 -> 실수하기 딱 좋음)
-    public static double apply(int op, double x, double y) {
-        switch (op) {
-            case PLUS:   return x + y;
-            case MINUS:  return x - y;
-            case TIMES:  return x * y;
-            case DIVIDE: return x / y;
-            default: throw new AssertionError("Unknown op: " + op);
-        }
+    public double apply(double x, double y) {
+        return op.applyAsDouble(x, y);
     }
 }
---
+++ src/test/java/org/jsh/chapter05/item34/OperationTest.java
@@ -0,0 +1,25 @@
+package org.jsh.chapter05.item34;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class OperationTest {
+
+    @Test
+    @DisplayName("정수 상수 패턴의 계산기 테스트")
+    void operationTest() {
+        // Given
+        double x = 2.0;
+        double y = 4.0;
+
+        // When & Then
+        assertThat(Operation.PLUS.apply(x, y)).isEqualTo(6.0);
+        assertThat(Operation.DIVIDE.apply(x, y)).isEqualTo(0.5);
+
+        // Bad Case: 존재하지 않는 연산(99)을 넣어도 컴파일은 됨 (타입 안전성 X)
+        // 실행해야 비로소 에러 발생
+        //assertThatThrownBy(() -> Operation.apply(99, x, y))
+        //        .isInstanceOf(AssertionError.class);
+    }
+}
