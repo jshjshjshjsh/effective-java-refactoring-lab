a0adc5ea40b75a3f8488d497bd8a0dfed153b828
jshjshjshjsh <cj456456@gmail.com>
2025-12-31 23:23:02 +0900

    refactor(AI): Stream으로 리펙토링

--- src/main/java/org/jsh/chapter08/item55/Max.java
+++ src/main/java/org/jsh/chapter08/item55/Max.java
@@ -1,6 +1,7 @@
 package org.jsh.chapter08.item55;
 
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -17,5 +18,9 @@ public class Max {
                 result = Objects.requireNonNull(e);
         }
         return Optional.of(result);
+    }
+
+    public static <E extends Comparable<E>> Optional<E> maxStream(Collection<E> c) {
+        return c.stream().max(Comparator.naturalOrder());
     }
 }
--- src/test/java/org/jsh/chapter08/item55/MaxTest.java
+++ src/test/java/org/jsh/chapter08/item55/MaxTest.java
@@ -24,4 +24,20 @@ class MaxTest {
         //        .hasMessage("빈 컬렉션입니다.");
         assertThat(Max.max(List.<Integer>of())).isEqualTo(Optional.empty());
     }
+
+    @Test
+    @DisplayName("최댓값 구하기 및 빈 컬렉션 예외 확인(Stream 적용)")
+    void maxStreamTest() {
+        // Given
+        List<Integer> list = List.of(1, 3, 2);
+
+        // When & Then (정상 케이스)
+        assertThat(Max.maxStream(list)).contains(3);
+
+        // When & Then (예외 케이스)
+        //assertThatThrownBy(() -> Max.max(List.<Integer>of()))
+        //        .isInstanceOf(IllegalArgumentException.class)
+        //        .hasMessage("빈 컬렉션입니다.");
+        assertThat(Max.maxStream(List.<Integer>of())).isEqualTo(Optional.empty());
+    }
 }
