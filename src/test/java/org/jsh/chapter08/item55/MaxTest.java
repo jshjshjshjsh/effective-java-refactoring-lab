3e96384bd76ab58759df39f8d2d482085f14abbf
jshjshjshjsh <cj456456@gmail.com>
2025-12-31 23:05:40 +0900

    test: add Test code

---
+++ src/test/java/org/jsh/chapter08/item55/MaxTest.java
@@ -0,0 +1,27 @@
+package org.jsh.chapter08.item55;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MaxTest {
+
+    @Test
+    @DisplayName("최댓값 구하기 및 빈 컬렉션 예외 확인")
+    void maxTest() {
+        // Given
+        List<Integer> list = List.of(1, 3, 2);
+
+        // When & Then (정상 케이스)
+        assertThat(Max.max(list)).isEqualTo(3);
+
+        // When & Then (예외 케이스)
+        // Before: 빈 리스트를 넣으면 예외가 발생해야 함
+        assertThatThrownBy(() -> Max.max(List.<Integer>of()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("빈 컬렉션입니다.");
+    }
+}
