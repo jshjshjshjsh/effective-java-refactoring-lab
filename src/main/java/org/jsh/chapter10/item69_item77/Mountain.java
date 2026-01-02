d4e33e9ecc17bf52991741fe2c86200538c27718
jshjshjshjsh <cj456456@gmail.com>
2026-01-02 19:47:52 +0900

    refactor: foreach, exception에 코드 추가

--- src/main/java/org/jsh/chapter10/item69_item77/Mountain.java
+++ src/main/java/org/jsh/chapter10/item69_item77/Mountain.java
@@ -2,15 +2,10 @@ package org.jsh.chapter10.item69_item77;
 
 public class Mountain {
 
-    // Item 69 위반: 예외를 반복문 종료용으로 사용 (끔찍함)
+    // Item 69 위반: 예외를 반복문 종료용으로 사용
     public static void climb(int[] mountains) {
-        try {
-            int i = 0;
-            while (true) {
-                System.out.println("등반 중: 높이 " + mountains[i++]);
-            }
-        } catch (ArrayIndexOutOfBoundsException e) {
-            // 배열 끝에 도달하면 예외가 발생하고 여기서 루프 종료
+        for ( int height : mountains ) {
+            System.out.println("등반 중: 높이 " + height);
         }
     }
 
@@ -19,7 +14,8 @@ public class Mountain {
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
-            // 아무것도 안 함 (예외를 꿀꺽 삼킴 -> 디버깅 지옥의 시작)
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
         }
     }
 }
