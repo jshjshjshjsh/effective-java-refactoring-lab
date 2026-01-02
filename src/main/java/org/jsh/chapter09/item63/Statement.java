44821a95681b758eec153462c3bc21ce3d452877
jshjshjshjsh <cj456456@gmail.com>
2026-01-02 19:08:23 +0900

    feat: add Before code

---
+++ src/main/java/org/jsh/chapter09/item63/Statement.java
@@ -0,0 +1,15 @@
+package org.jsh.chapter09.item63;
+
+import java.util.List;
+
+public class Statement {
+
+    // Bad: 반복문 내에서 문자열 연결 (+) 연산자 사용 -> 성능 최악 (O(n^2))
+    public static String makeLine(List<String> items) {
+        String result = "";
+        for (String item : items) {
+            result += item; // 여기가 병목 지점! 매번 새 String 객체를 생성하고 복사함
+        }
+        return result;
+    }
+}
